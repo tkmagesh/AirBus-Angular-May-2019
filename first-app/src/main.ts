91b38e151fc1a38b272df55808dd8ea58926b635
Magesh <tkmagesh77@gmail.com>
2019-05-21 17:02:23 +0530

    first app - setup demo

---
+++ first-app/src/main.ts
@@ -0,0 +1,17 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+import { add } from './calculator';
+
+add(10,20);
+
+if (environment.production) {
+  enableProdMode();
+}
+
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
